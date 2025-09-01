93546cd7aa8308b862d8a5079ec83272ee59edcc
aemerson1 <alexis.g.emerson@gmail.com>
2025-08-31 21:10:42 -0400

    some examples

--- lib/validate.py
+++ lib/validate.py
@@ -1,7 +1,11 @@
 def validate(json_data, json_schema) -> dict:
     
-    # Generate pydantic models from json schema
+    generate_models(json_schema)
 
     # Use pydantic to validate
     
     return {"success": True}
+
+def generate_models(json_schema):
+    # Generate pydantic models from json schema
+    return
