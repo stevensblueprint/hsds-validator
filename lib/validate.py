be18fd24619832218a164e4a79db40355ede3e7b
Shiven Pandya <shiven.pandya@peer-consulting.com>
2025-09-11 22:19:22 -0400

    refactor: enhance validation function with pydantic model integration

--- lib/validate.py
+++ lib/validate.py
@@ -1,10 +1,27 @@
-def validate(json_data, json_schema) -> dict:
-    
-    generate_models(json_schema)
+from pydantic import BaseModel, ValidationError
+import json
 
-    # Use pydantic to validate
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+
+def validate(json_data: dict, json_schema: dict) -> dict:
+    
+    model = generate_models(json_schema)
+    json_data_str = json.dumps(json_data)
+    
+    try:
+        model.model_validate_json(json_data_str)
+    except ValidationError as e:
+        error_dict = e.errors()
+        err_message = []
+        for error in error_dict:
+            err_message.append({"column": error['loc'][0], "message": error['msg']})
+        return {"success": False, "errors": err_message}
     
     return {"success": True}
 
-def generate_models(json_schema):
+def generate_models(json_schema) -> BaseModel:
     # Generate pydantic models from json schema
+    return User
